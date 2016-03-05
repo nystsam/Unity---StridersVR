55fca16f5db26427e22e471fb7df787910f992c6
dsam <sam.nyst@hotmail.com>
2016-03-05 02:06:07 -0430

    otro cmmit

---
+++ Striders VR/Assets/src/Modules/Training-TrainOfThought/Classes/Controller/BoundingBoxButtonController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundingBoxButtonController : MonoBehaviour {
+
+	public float alphaSmoothValue;
+	public GameObject buttonRenderer;
+	
+	private bool isTriggered = false;
+	private bool changeColorDone = true;
+	private Material myMaterial;
+	private Color myColor;
+
+
+	#region Script
+	void Start () 
+	{
+		myMaterial = buttonRenderer.GetComponent<MeshRenderer> ().material;
+		myColor = myMaterial.color;
+		myMaterial.color = new Color (myColor.r, myColor.g, myColor.b, 0.2f);
+	}
+
+	void Update () 
+	{
+		this.setMaterialColor();
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag.Equals ("Player")) {
+			if (!isTriggered) {
+				this.isTriggered = true;
+				changeColorDone = false;
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag.Equals ("Player")) {
+			if (isTriggered) {
+				this.isTriggered = false;
+				changeColorDone = false;
+			}
+		}
+	}
+	#endregion
+
+	private void setMaterialColor()
+	{
+		if (isTriggered && !changeColorDone) {
+			this.changeColor (alphaSmoothValue, 1, 1);
+		} 
+		else if (!isTriggered && !changeColorDone) {
+			this.changeColor (-alphaSmoothValue, 0.2f, -1);
+		}
+	}
+
+	private void changeColor(float localAlphaValueSmooth, float valueToStop, float numericDirection)
+	{
+		float currentAlpha = myMaterial.color.a;
+		valueToStop = valueToStop * numericDirection;
+		if((currentAlpha * numericDirection) <= valueToStop)
+			myMaterial.color = new Color(myColor.r, myColor.g, myColor.b, currentAlpha + localAlphaValueSmooth);
+		else
+			changeColorDone = true;	
+	}
+}
---
+++ Striders VR/Assets/src/Modules/Training-TrainOfThought/Classes/Controller/SwitchButtonController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchButtonController : MonoBehaviour {
+
+	public float spring;
+	public float triggerDistance;
+	public float max_distance;
+	public float min_distance;
+	public GameObject pressedAddon;
+	
+	private bool is_pressed;
+	private Vector3 resting_position;
+	private Material childMaterial;
+
+	#region Script
+	void Start()
+	{
+		this.resting_position = transform.localPosition;
+		this.is_pressed = false;
+		childMaterial = pressedAddon.GetComponent<MeshRenderer>().material;
+	}
+	
+	void Update()
+	{
+		this.constraintMovement();
+		this.appliySpring ();
+		this.checkTrigger ();
+	}
+	#endregion
+
+	private void constraintMovement()
+	{
+		Vector3 _local_position = transform.localPosition;
+		_local_position.y = Mathf.Clamp (_local_position.y, this.min_distance, this.max_distance);
+		transform.localPosition = _local_position;
+	}
+	
+	private void appliySpring()
+	{
+		transform.GetComponent<Rigidbody> ().AddRelativeForce (-this.spring * (transform.localPosition - this.resting_position));
+	}
+
+	private void checkTrigger()
+	{
+		if (!this.is_pressed) {
+			if (transform.localPosition.y < this.triggerDistance) {
+				this.buttonPressed(1);
+				this.is_pressed = true;
+			}
+		} else if (this.is_pressed) {
+			if (transform.localPosition.y > this.triggerDistance) {
+				this.buttonPressed(0);
+				this.is_pressed = false;
+			}
+		}
+	}
+
+	private void buttonPressed(float alpha)
+	{
+		childMaterial.color = new Color(childMaterial.color.r,childMaterial.color.g, childMaterial.color.b, alpha);
+
+	}
+
+}
